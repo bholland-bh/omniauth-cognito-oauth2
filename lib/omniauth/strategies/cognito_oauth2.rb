166e06a5a40ea001bc60e2bb0bc98c9b3f2a759c
Adam Wenham <adamwenham64@gmail.com>
2020-01-03 15:59:08 +0100

    initial commit with updated file structure to match google-oauth2 gem

---
+++ lib/omniauth/strategies/cognito_oauth2.rb
@@ -0,0 +1,10 @@
+require "omniauth/cognito/oauth2/version"
+
+module Omniauth
+  module Cognito
+    module Oauth2
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
